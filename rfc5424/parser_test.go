184304e335d3c06f7299b5918ca750c8ede5dbe7
Leonardo Di Donato <leodidonato@gmail.com>
2018-03-30 14:19:53 +0200

    New: Setup testing table for tests & benchmarks
    Co-authored-by: Lorenzo Fontana <lo@linux.com>

--- rfc5424/parser_test.go
+++ rfc5424/parser_test.go
@@ -3,26 +3,70 @@ package rfc5424
 import (
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
 )
 
 type testCase struct {
 	input string
 	valid bool
-	// value SyslogMessage
+	value *SyslogMessage
 }
 
-func TestDumb(t *testing.T) {
-	line := "<101>122 "
-	msg, err := Parse(line)
-	if err != nil {
-		t.Error(err)
-		return
+var testCases = []testCase{
+	{
+		"<101>122 201-11-22",
+		false,
+		nil,
+	},
+	{
+		"<101>122 2018-11-22",
+		true,
+		&SyslogMessage{
+			Header: Header{
+				Pri: Pri{
+					Prival: Prival{
+						Facility: Facility{
+							Code: 12,
+						},
+						Severity: Severity{
+							Code: 5,
+						},
+						Value: 101,
+					},
+				},
+				Version: Version{
+					Value: 122,
+				},
+			},
+		},
+	},
+}
+
+func TestParse(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			t.Parallel()
+
+			msg, err := Parse(tc.input)
+			if !tc.valid {
+				assert.Nil(t, msg)
+				assert.Error(t, err)
+			}
+			if tc.valid {
+				assert.Nil(t, err)
+				assert.NotEmpty(t, msg)
+			}
+			assert.Equal(t, tc.value, msg)
+		})
 	}
+}
 
-	spew.Dump(msg)
-
-	// if msg.Prival.Value != 101 {
-	// 	t.Errorf("Prival value not matching")
-	// }
+func BenchmarkParse(b *testing.B) {
+	for _, tc := range testCases {
+		b.Run(tc.input, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Parse(tc.input)
+			}
+		})
+	}
 }
