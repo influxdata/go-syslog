32f793e71529bfb6cbdc0f1b553e02df4bede2ca
Leonardo Di Donato <leodidonato@gmail.com>
2018-04-06 19:16:51 +0200

    Update: Unsage decimal code points reduction to integer

--- chars/functions_test.go
+++ chars/functions_test.go
@@ -8,28 +8,25 @@ import (
 
 func TestSimpleUTF8DecimalConversion(t *testing.T) {
 	slice := []uint8{49, 48, 49}
-	res, err := UTF8DecimalCodePointsToInt(slice)
-	assert.Nil(t, err)
-	assert.Equal(t, 101, *res)
+	res := UnsafeUTF8DecimalCodePointsToInt(slice)
+	assert.Equal(t, 101, res)
 }
 
 func TestNumberStartingWithZero(t *testing.T) {
 	slice := []uint8{48, 48, 50}
-	res, err := UTF8DecimalCodePointsToInt(slice)
-	assert.Nil(t, err)
-	assert.Equal(t, 2, *res)
+	res := UnsafeUTF8DecimalCodePointsToInt(slice)
+	assert.Equal(t, 2, res)
 }
 
-func TestNonNumberChars(t *testing.T) {
-	slice := []uint8{10} // Line Feed (LF)
-	res, err := UTF8DecimalCodePointsToInt(slice)
-	assert.NotNil(t, err)
-	assert.Nil(t, res)
+func TestCharsNotInRange(t *testing.T) {
+	point := 10
+	slice := []uint8{uint8(point)} // Line Feed (LF)
+	res := UnsafeUTF8DecimalCodePointsToInt(slice)
+	assert.Equal(t, res, -(48 - point))
 }
 
 func TestAllDigits(t *testing.T) {
 	slice := []uint8{49, 50, 51, 52, 53, 54, 55, 56, 57, 48}
-	res, err := UTF8DecimalCodePointsToInt(slice)
-	assert.Nil(t, err)
-	assert.Equal(t, 1234567890, *res)
+	res := UnsafeUTF8DecimalCodePointsToInt(slice)
+	assert.Equal(t, 1234567890, res)
 }
