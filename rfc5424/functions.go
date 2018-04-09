16ce79771780ac106d8b26b090e9451a866394ca
Leonardo Di Donato <leodidonato@gmail.com>
2018-04-09 18:28:00 +0200

    Update: Helper functions are now private

--- rfc5424/functions.go
+++ rfc5424/functions.go
@@ -1,11 +1,11 @@
-package chars
+package rfc5424
 
-// UnsafeUTF8DecimalCodePointsToInt converts a slice containing
+// unsafeUTF8DecimalCodePointsToInt converts a slice containing
 // a series of UTF-8 decimal code points into their integer rapresentation.
 //
 // It assumes input code points are in the range 48-57.
 // Returns a pointer since an empty slice is equal to nil and not to the zero value of the codomain (ie., `int`).
-func UnsafeUTF8DecimalCodePointsToInt(chars []uint8) int {
+func unsafeUTF8DecimalCodePointsToInt(chars []uint8) int {
 	out := 0
 	ord := 1
 	for i := len(chars) - 1; i >= 0; i-- {
