5e1ca706f8f018af4d5ae757a9aba6100316d2b5
Leonardo Di Donato <leodidonato@gmail.com>
2018-04-10 14:27:51 +0200

    Update: Test suite

--- rfc5424/machine_test.go
+++ rfc5424/machine_test.go
@@ -18,9 +18,9 @@ func getStringAddress(str string) *string {
 
 func createName(x []byte) string {
 	str := string(x)
-	lim := 30
+	lim := 60
 	if len(str) > lim {
-		return string(str[0:30])
+		return string(str[0:lim])
 	}
 	return str
 }
@@ -120,61 +120,400 @@ var testCases = []testCase{
 		"expecting a RFC3339 or a RFC3339NANO timestamp or a nil value [col 18]",
 	},
 	// Non existing date
+	// (fixme) > removing last space we get nil, not the parse timestamp error
 	{
-		[]byte("<34>11 2003-02-30T05:14:15.000003-07:00 "), // (fixme) > needed space here ...
+		[]byte("<34>11 2003-09-31T22:14:15.003Z "),
 		false,
 		nil,
-		"parsing time \"2003-02-30T05:14:15.000003-07:00\": day out of range",
+		"parsing time \"2003-09-31T22:14:15.003Z\": day out of range",
 	},
-	// All right but without structured data
+	// valid
+	{
+		[]byte("<1>1 - - - - - -"),
+		true,
+		&SyslogMessage{
+			facility: 0,
+			severity: 1,
+			Priority: 1,
+			Version:  1,
+		},
+		"",
+	},
+	// valid, w/o structure data, w/0 procid
 	{
 		[]byte("<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8"),
 		true,
 		&SyslogMessage{
-			Header: Header{
-				Pri: Pri{
-					Prival: Prival{
-						Facility: 4,
-						Severity: 2,
-						Value:    34,
-					},
-				},
-				Version:   1,
-				Timestamp: timeParse(time.RFC3339Nano, "2003-10-11T22:14:15.003Z"),
-				Hostname:  getStringAddress("mymachine.example.com"),
-				Appname:   getStringAddress("su"),
-				ProcID:    nil,
-				MsgID:     getStringAddress("ID47"),
-			},
+			facility:       4,
+			severity:       2,
+			Priority:       34,
+			Version:        1,
+			Timestamp:      timeParse(time.RFC3339Nano, "2003-10-11T22:14:15.003Z"),
+			Hostname:       getStringAddress("mymachine.example.com"),
+			Appname:        getStringAddress("su"),
+			ProcID:         nil,
+			MsgID:          getStringAddress("ID47"),
 			StructuredData: nil,
 			Message:        getStringAddress("BOM'su root' failed for lonvick on /dev/pts/8"),
 		},
 		"",
 	},
-	// Nil timestamp
+	// valid, w/o structure data, w/o timestamp
 	{
-		[]byte("<187>222 - mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8"),
+		[]byte("<187>222 - mymachine.example.com su - ID47 - 'su root' failed for lonvick on /dev/pts/8"),
 		true,
 		&SyslogMessage{
-			Header: Header{
-				Pri: Pri{
-					Prival: Prival{
-						Facility: 23,
-						Severity: 3,
-						Value:    187,
-					},
-				},
-				Version:   222,
-				Timestamp: nil,
-				Hostname:  getStringAddress("mymachine.example.com"),
-				Appname:   getStringAddress("su"),
-				ProcID:    nil,
-				MsgID:     getStringAddress("ID47"),
-			},
+			facility:       23,
+			severity:       3,
+			Priority:       187,
+			Version:        222,
+			Timestamp:      nil,
+			Hostname:       getStringAddress("mymachine.example.com"),
+			Appname:        getStringAddress("su"),
+			ProcID:         nil,
+			MsgID:          getStringAddress("ID47"),
 			StructuredData: nil,
-			Message:        getStringAddress("BOM'su root' failed for lonvick on /dev/pts/8"),
+			Message:        getStringAddress("'su root' failed for lonvick on /dev/pts/8"),
 		},
 		"",
+	},
+	// valid, w/o structure data, w/o msgid
+	{
+		[]byte("<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% Time to make the do-nuts."),
+		true,
+		&SyslogMessage{
+			facility:       20,
+			severity:       5,
+			Priority:       165,
+			Version:        1,
+			Timestamp:      timeParse(time.RFC3339Nano, "2003-08-24T05:14:15.000003-07:00"),
+			Hostname:       getStringAddress("192.0.2.1"),
+			Appname:        getStringAddress("myproc"),
+			ProcID:         getStringAddress("8710"),
+			MsgID:          nil,
+			StructuredData: nil,
+			Message:        getStringAddress("%% Time to make the do-nuts."),
+		},
+		"",
+	},
+	// valid, w/o structure data, w/o hostname, w/o appname, w/o procid, w/o msgid, w/o msg
+	{
+		[]byte("<165>2 2003-08-24T05:14:15.000003-07:00 - - - - -"),
+		true,
+		&SyslogMessage{
+			facility:       20,
+			severity:       5,
+			Priority:       165,
+			Version:        2,
+			Timestamp:      timeParse(time.RFC3339Nano, "2003-08-24T05:14:15.000003-07:00"),
+			Hostname:       nil,
+			Appname:        nil,
+			ProcID:         nil,
+			MsgID:          nil,
+			StructuredData: nil,
+			Message:        nil,
+		},
+		"",
+	},
+	// valid, w/o structure data, w/o hostname, w/o appname, w/o procid, w/o msgid, empty msg
+	{
+		[]byte("<165>2 2003-08-24T05:14:15.000003-07:00 - - - - - "),
+		true,
+		&SyslogMessage{
+			facility:       20,
+			severity:       5,
+			Priority:       165,
+			Version:        2,
+			Timestamp:      timeParse(time.RFC3339Nano, "2003-08-24T05:14:15.000003-07:00"),
+			Hostname:       nil,
+			Appname:        nil,
+			ProcID:         nil,
+			MsgID:          nil,
+			StructuredData: nil,
+			Message:        nil,
+		},
+		"",
+	},
+	// valid, with structured data is, w/o structured data params
+	{
+		[]byte("<78>1 2016-01-15T00:04:01+00:00 host1 CROND 10391 - [sdid] some_message"),
+		true,
+		&SyslogMessage{
+			facility:  9,
+			severity:  6,
+			Priority:  78,
+			Version:   1,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T00:04:01+00:00"),
+			Hostname:  getStringAddress("host1"),
+			Appname:   getStringAddress("CROND"),
+			ProcID:    getStringAddress("10391"),
+			MsgID:     nil,
+			StructuredData: &map[string]map[string]string{
+				"sdid": map[string]string{},
+			},
+			Message: getStringAddress("some_message"),
+		},
+		"",
+	},
+	// valid, with structured data is, wit structured data params
+	{
+		[]byte(`<78>1 2016-01-15T00:04:01+00:00 host1 CROND 10391 - [sdid x="⌘"] some_message`),
+		true,
+		&SyslogMessage{
+			facility:  9,
+			severity:  6,
+			Priority:  78,
+			Version:   1,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T00:04:01+00:00"),
+			Hostname:  getStringAddress("host1"),
+			Appname:   getStringAddress("CROND"),
+			ProcID:    getStringAddress("10391"),
+			MsgID:     nil,
+			StructuredData: &map[string]map[string]string{
+				"sdid": map[string]string{
+					"x": "⌘",
+				},
+			},
+			Message: getStringAddress("some_message"),
+		},
+		"",
+	},
+	// valid, with structured data is, wit structured data params
+	{
+		[]byte(`<78>2 2016-01-15T00:04:01+00:00 host1 CROND 10391 - [sdid x="hey \\u2318 hey"] some_message`),
+		true,
+		&SyslogMessage{
+			facility:  9,
+			severity:  6,
+			Priority:  78,
+			Version:   2,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T00:04:01+00:00"),
+			Hostname:  getStringAddress("host1"),
+			Appname:   getStringAddress("CROND"),
+			ProcID:    getStringAddress("10391"),
+			MsgID:     nil,
+			StructuredData: &map[string]map[string]string{
+				"sdid": map[string]string{
+					"x": `hey \u2318 hey`,
+				},
+			},
+			Message: getStringAddress("some_message"),
+		},
+		"",
+	},
+	// valid, with (escaped) backslash within structured data param value
+	{
+		[]byte(`<29>50 2016-01-15T01:00:43Z hn S - - [meta es="\\valid"] 127.0.0.1 - - 1452819643 "GET"`),
+		true,
+		&SyslogMessage{
+			Priority:  29,
+			facility:  3,
+			severity:  5,
+			Version:   50,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T01:00:43Z"),
+			Hostname:  getStringAddress("hn"),
+			Appname:   getStringAddress("S"),
+			StructuredData: &map[string]map[string]string{
+				"meta": map[string]string{
+					"es": `\valid`,
+				},
+			},
+			Message: getStringAddress(`127.0.0.1 - - 1452819643 "GET"`),
+		},
+		"",
+	},
+	// valid, with control characters within structured data param value
+	{
+		[]byte("<29>50 2016-01-15T01:00:43Z hn S - - [meta es=\"\b5Ὂg̀9! ℃ᾭG\"] 127.0.0.1 - - 1452819643 \"GET\""),
+		true,
+		&SyslogMessage{
+			Priority:  29,
+			facility:  3,
+			severity:  5,
+			Version:   50,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T01:00:43Z"),
+			Hostname:  getStringAddress("hn"),
+			Appname:   getStringAddress("S"),
+			StructuredData: &map[string]map[string]string{
+				"meta": map[string]string{
+					"es": "\b5Ὂg̀9! ℃ᾭG",
+				},
+			},
+			Message: getStringAddress(`127.0.0.1 - - 1452819643 "GET"`),
+		},
+		"",
+	},
+	// valid, with structured data, w/o msg
+	{
+		[]byte("<165>3 2003-10-11T22:14:15.003Z example.com evnts - ID27 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"]"),
+		true,
+		&SyslogMessage{
+			facility:  20,
+			severity:  5,
+			Priority:  165,
+			Version:   3,
+			Timestamp: timeParse(time.RFC3339Nano, "2003-10-11T22:14:15.003Z"),
+			Hostname:  getStringAddress("example.com"),
+			Appname:   getStringAddress("evnts"),
+			ProcID:    nil,
+			MsgID:     getStringAddress("ID27"),
+			StructuredData: &map[string]map[string]string{
+				"exampleSDID@32473": map[string]string{
+					"iut":         "3",
+					"eventSource": "Application",
+					"eventID":     "1011",
+				},
+				"examplePriority@32473": map[string]string{
+					"class": "high",
+				},
+			},
+			Message: nil,
+		},
+		"",
+	},
+	// invalid, with duplicated structured data id
+	{
+		[]byte("<165>3 2003-10-11T22:14:15.003Z example.com evnts - ID27 [id1][id1]"),
+		false,
+		nil,
+		"duplicate structured data element id [col 66]",
+	},
+	// invalid, with duplicated structured data id
+	{
+		[]byte("<165>3 2003-10-11T22:14:15.003Z example.com evnts - ID27 [dupe e=\"1\"][id1][dupe class=\"l\"]"),
+		false,
+		nil,
+		"duplicate structured data element id [col 79]",
+	},
+	// valid, with structured data w/o msg
+	{
+		[]byte("<165>4 2003-10-11T22:14:15.003Z mymachine.it e - 1 [ex@32473 iut=\"3\" eventSource=\"A\"] An application event log entry..."),
+		true,
+		&SyslogMessage{
+			facility:  20,
+			severity:  5,
+			Priority:  165,
+			Version:   4,
+			Timestamp: timeParse(time.RFC3339Nano, "2003-10-11T22:14:15.003Z"),
+			Hostname:  getStringAddress("mymachine.it"),
+			Appname:   getStringAddress("e"),
+			ProcID:    nil,
+			MsgID:     getStringAddress("1"),
+			StructuredData: &map[string]map[string]string{
+				"ex@32473": map[string]string{
+					"iut":         "3",
+					"eventSource": "A",
+				},
+			},
+			Message: getStringAddress("An application event log entry..."),
+		},
+		"",
+	},
+	// valid, with double quotes in the message
+	{
+		[]byte(`<29>1 2016-01-15T01:00:43Z some-host-name SEKRETPROGRAM prg - [origin x-service="svcname"][meta sequenceId="1"] 127.0.0.1 - - 1452819643 "GET"`),
+		true,
+		&SyslogMessage{
+			facility:  3,
+			severity:  5,
+			Priority:  29,
+			Version:   1,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T01:00:43Z"),
+			Hostname:  getStringAddress("some-host-name"),
+			Appname:   getStringAddress("SEKRETPROGRAM"),
+			ProcID:    getStringAddress("prg"),
+			MsgID:     nil,
+			StructuredData: &map[string]map[string]string{
+				"origin": map[string]string{
+					"x-service": "svcname",
+				},
+				"meta": map[string]string{
+					"sequenceId": "1",
+				},
+			},
+			Message: getStringAddress("127.0.0.1 - - 1452819643 \"GET\""),
+		},
+		"",
+	},
+	// valid, with double quotes in the message and escaped character within param
+	{
+		[]byte(`<29>2 2016-01-15T01:00:43Z some-host-name SEKRETPROGRAM prg - [meta escape="\]"] 127.0.0.1 - - 1452819643 "GET"`),
+		true,
+		&SyslogMessage{
+			facility:  3,
+			severity:  5,
+			Priority:  29,
+			Version:   2,
+			Timestamp: timeParse(time.RFC3339Nano, "2016-01-15T01:00:43Z"),
+			Hostname:  getStringAddress("some-host-name"),
+			Appname:   getStringAddress("SEKRETPROGRAM"),
+			ProcID:    getStringAddress("prg"),
+			MsgID:     nil,
+			StructuredData: &map[string]map[string]string{
+				"meta": map[string]string{
+					"escape": "\\]", // (todo) > verify output is correct to the input param value
+				},
+			},
+			Message: getStringAddress("127.0.0.1 - - 1452819643 \"GET\""),
+		},
+		"",
+	},
+	// invalid, param value can not contain closing square bracket - ie., ]
+	{
+		[]byte(`<29>3 2016-01-15T01:00:43Z hn S - - [meta escape="]"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting chars `]`, `\"`, and `\\` to be escaped within param value [col 50]",
+	},
+	{
+		[]byte(`<29>3 2016-01-15T01:00:43Z hn S - - [meta escape="]q"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting chars `]`, `\"`, and `\\` to be escaped within param value [col 50]",
+	},
+	{
+		[]byte(`<29>3 2016-01-15T01:00:43Z hn S - - [meta escape="p]"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting chars `]`, `\"`, and `\\` to be escaped within param value [col 51]",
+	},
+	// invalid, param value can not contain doublequote char - ie., ""
+	{
+		[]byte(`<29>4 2016-01-15T01:00:43Z hn S - - [meta escape="""] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting a structured data parameter (`key=\"value\"`, both part from 1 to max 32 US-ASCII characters; key cannot contain `=`, ` `, `]`, and `\"`, while value cannot contain `]`, backslash, and `\"` unless escaped) [col 51]",
+	},
+	{
+		[]byte(`<29>4 2016-01-15T01:00:43Z hn S - - [meta escape="a""] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting a structured data parameter (`key=\"value\"`, both part from 1 to max 32 US-ASCII characters; key cannot contain `=`, ` `, `]`, and `\"`, while value cannot contain `]`, backslash, and `\"` unless escaped) [col 52]",
+	},
+	{
+		[]byte(`<29>4 2016-01-15T01:00:43Z hn S - - [meta escape=""b"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting a structured data parameter (`key=\"value\"`, both part from 1 to max 32 US-ASCII characters; key cannot contain `=`, ` `, `]`, and `\"`, while value cannot contain `]`, backslash, and `\"` unless escaped) [col 51]",
+	},
+	// invalid, param value can not contain backslash - ie., \
+	{
+		[]byte(`<29>5 2016-01-15T01:00:43Z hn S - - [meta escape="\"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting chars `]`, `\"`, and `\\` to be escaped within param value [col 52]",
+	},
+	{
+		[]byte(`<29>5 2016-01-15T01:00:43Z hn S - - [meta escape="a\"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting chars `]`, `\"`, and `\\` to be escaped within param value [col 53]",
+	},
+	{
+		[]byte(`<29>5 2016-01-15T01:00:43Z hn S - - [meta escape="\n"] 127.0.0.1 - - 1452819643 "GET"`),
+		false,
+		nil,
+		"expecting chars `]`, `\"`, and `\\` to be escaped within param value [col 51]",
 	},
 }
 
