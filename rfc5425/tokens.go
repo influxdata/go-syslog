c7bad67f58d8f52169d091962be1e891f1b56b07
Leonardo Di Donato <leodidonato@gmail.com>
2018-04-26 15:44:00 +0200

    New: String methods for tokens

--- rfc5425/tokens.go
+++ rfc5425/tokens.go
@@ -1,11 +1,13 @@
 package rfc5425
 
-//go:generate stringer -type TokenType $GOFILE
+import (
+	"strconv"
+)
 
 // Token represents a lexical token
 type Token struct {
 	typ TokenType
-	lit string
+	lit []byte
 }
 
 // TokenType represents a lexical token type
@@ -19,3 +21,25 @@ const (
 	MSGLEN
 	SYSLOGMSG
 )
+
+// String outputs the string representation of the receiving Token
+func (t Token) String() string {
+	switch t.typ {
+	case WS, EOF:
+		return "Token{" + t.typ.String() + "}"
+	default:
+		return "Token{" + t.typ.String() + ", " + string(t.lit) + "}"
+	}
+}
+
+const tokentypename = "ILLEGALEOFWSMSGLENSYSLOGMSG"
+
+var tokentypeindex = [...]uint8{0, 7, 10, 12, 18, 27}
+
+// String outputs the string representation of the receiving TokenType
+func (i TokenType) String() string {
+	if i < 0 || i >= TokenType(len(tokentypeindex)-1) {
+		return "TokenType(" + strconv.FormatInt(int64(i), 10) + ")"
+	}
+	return tokentypename[tokentypeindex[i]:tokentypeindex[i+1]]
+}
