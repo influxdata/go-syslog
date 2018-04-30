f5e1246fe9208c0095cdc354676c65453009fc53
Leonardo Di Donato <leodidonato@gmail.com>
2018-04-30 22:02:31 +0200

    New: RFC5425 test suite

--- rfc5425/parser_test.go
+++ rfc5425/parser_test.go
@@ -1,34 +1,201 @@
 package rfc5425
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/influxdata/go-syslog/rfc5424"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestAbc(t *testing.T) {
-	// r := strings.NewReader("16 <1>1 - - - - - -17 <2>12 A B C D E -")
+func getStringAddress(str string) *string {
+	return &str
+}
 
-	// r := strings.NewReader("16 <1>1 A B C D E -xaaa") // longer that MSGLEN
+func getUint8Address(x uint8) *uint8 {
+	return &x
+}
 
-	// what to do in such situation?
-	// message is minimally valid but it does not respect MSGLEN
-	// discard?
-	// r := strings.NewReader("16 <1>1") // shorter than MSGLEN
+type testCase struct {
+	descr    string
+	input    string
+	results  []Result
+	pResults []Result
+}
 
-	r := strings.NewReader("23 <1>1 - - - - - - hellø") // msglen is the octet count
+func getTimestampError(col int) error {
+	return fmt.Errorf("expecting a RFC3339 or a RFC3339NANO timestamp or a nil value [col %d]", col)
+}
 
-	results := NewParser(r, true).Parse()
-	//fmt.Printf("%T\n", results)
+func getParsingError(col int) error {
+	return fmt.Errorf("parsing error [col %d]", col)
+}
 
-	for r := range results {
-		spew.Dump(r)
+var testCases = []testCase{
+	{
+		"empty",
+		"",
+		[]Result{
+			Result{Error: fmt.Errorf("found %s, expecting a %s", EOF, MSGLEN)},
+		},
+		[]Result{
+			Result{Error: fmt.Errorf("found %s, expecting a %s", EOF, MSGLEN)},
+		},
+	},
+	{
+		"1st ok/2nd mf", // mf means malformed syslog message
+		"16 <1>1 - - - - - -17 <2>12 A B C D E -",
+		// results w/o best effort
+		[]Result{
+			Result{
+				Message: (&rfc5424.SyslogMessage{}).SetPriority(1).SetVersion(1),
+			},
+			Result{
+				MessageError: getTimestampError(6),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Message: (&rfc5424.SyslogMessage{}).SetPriority(1).SetVersion(1),
+			},
+			Result{
+				Message:      (&rfc5424.SyslogMessage{}).SetPriority(2).SetVersion(12),
+				MessageError: getTimestampError(6),
+			},
+		},
+	},
+	{
+		"1st ok/2nd ko", // ko means wrong token
+		"16 <1>1 - - - - - -xaaa",
+		// results w/o best effort
+		[]Result{
+			Result{
+				Message: (&rfc5424.SyslogMessage{}).SetPriority(1).SetVersion(1),
+			},
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", Token{ILLEGAL, []byte("x")}, MSGLEN),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Message: (&rfc5424.SyslogMessage{}).SetPriority(1).SetVersion(1),
+			},
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", Token{ILLEGAL, []byte("x")}, MSGLEN),
+			},
+		},
+	},
+	{
+		"1st ml/2nd ko",
+		"16 <1>1 A B C D E -xaaa",
+		// results w/o best effort
+		[]Result{
+			Result{
+				MessageError: getTimestampError(5),
+			},
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", Token{ILLEGAL, []byte("x")}, MSGLEN),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Message:      (&rfc5424.SyslogMessage{}).SetPriority(1).SetVersion(1),
+				MessageError: getTimestampError(5),
+			},
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", Token{ILLEGAL, []byte("x")}, MSGLEN),
+			},
+		},
+	},
+	{
+		"1st ok/utf8",
+		"23 <1>1 - - - - - - hellø", // msglen MUST be the octet count
+		//results w/o best effort
+		[]Result{
+			Result{
+				Message: (&rfc5424.SyslogMessage{}).
+					SetPriority(1).
+					SetVersion(1).
+					SetMessage("hellø"),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Message: (&rfc5424.SyslogMessage{}).
+					SetPriority(1).
+					SetVersion(1).
+					SetMessage("hellø"),
+			},
+		},
+	},
+	{
+		"1st ko/incomplete/SYSLOGMSG",
+		"16 <1>1",
+		// results w/o best effort
+		[]Result{
+			Result{
+				Error: fmt.Errorf(`found %s after "%s", expecting a %s containing %d octets`, EOF, "<1>1", SYSLOGMSG, 16),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Message:      (&rfc5424.SyslogMessage{}).SetPriority(1).SetVersion(1),
+				MessageError: getParsingError(4),
+				Error:        fmt.Errorf(`found %s after "%s", expecting a %s containing %d octets`, EOF, "<1>1", SYSLOGMSG, 16),
+			},
+		},
+	},
+	{
+		"1st ko/missing/WS/found/ILLEGAL",
+		"16<1>1",
+		// results w/o best effort
+		[]Result{
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", Token{ILLEGAL, []byte("<")}, WS),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", Token{ILLEGAL, []byte("<")}, WS),
+			},
+		},
+	},
+	{
+		"1st ko/missing/WS/found/EOF",
+		"1",
+		// results w/o best effort
+		[]Result{
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", EOF, WS),
+			},
+		},
+		// results with best effort
+		[]Result{
+			Result{
+				Error: fmt.Errorf("found %s, expecting a %s", EOF, WS),
+			},
+		},
+	},
+}
+
+func TestParse(t *testing.T) {
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.descr, func(t *testing.T) {
+			// t.Parallel()
+
+			res1 := NewParser(strings.NewReader(tc.input), false).Parse()
+			res2 := NewParser(strings.NewReader(tc.input), true).Parse()
+
+			assert.Equal(t, tc.results, res1)
+			assert.Equal(t, tc.pResults, res2)
+		})
 	}
-
-	// what to do when error is a token error? (1) try to continue the parse? or (2)stop?
-	// in case (1) the parse will stop only when EOF encountered
-	// this choice is about strictiness ..
-
-	// do not return channel but passing it so the user can use buffered channels to bulk N messages?
 }
