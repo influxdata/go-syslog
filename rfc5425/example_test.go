424b194e9c09a347982f7e6ae9b91c14511549e5
Leonardo Di Donato <leodidonato@gmail.com>
2018-05-02 13:10:24 +0200

    Docs: Examples about RFC5425 parsing

---
+++ rfc5425/example_test.go
@@ -0,0 +1,88 @@
+package rfc5425
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ExampleNewParser() {
+	r := strings.NewReader("48 <1>1 2003-10-11T22:14:15.003Z host.local - - - -25 <3>1 - host.local - - - -38 <2>1 - host.local su - - - κόσμε")
+	NewParser(r, WithBestEffort()).Parse()
+	// Output: ([]rfc5425.Result) {
+	// (rfc5425.Result) {
+	// 	Message: (*rfc5424.SyslogMessage)({
+	// 	Priority: (*uint8)(1),
+	// 	facility: (*uint8)(0),
+	// 	severity: (*uint8)(1),
+	// 	Version: (uint16) 1,
+	// 	Timestamp: (*time.Time)(2003-10-11 22:14:15.003 +0000 UTC),
+	// 	Hostname: (*string)("host.local"),
+	// 	Appname: (*string)(<nil>),
+	// 	ProcID: (*string)(<nil>),
+	// 	MsgID: (*string)(<nil>),
+	// 	StructuredData: (*map[string]map[string]string)(<nil>),
+	// 	Message: (*string)(<nil>)
+	// 	}),
+	// 	MessageError: (error) <nil>,
+	// 	Error: (error) <nil>
+	// },
+	// (rfc5425.Result) {
+	// 	Message: (*rfc5424.SyslogMessage)({
+	// 	Priority: (*uint8)(3),
+	// 	facility: (*uint8)(0),
+	// 	severity: (*uint8)(3),
+	// 	Version: (uint16) 1,
+	// 	Timestamp: (*time.Time)(<nil>),
+	// 	Hostname: (*string)("host.local"),
+	// 	Appname: (*string)(<nil>),
+	// 	ProcID: (*string)(<nil>),
+	// 	MsgID: (*string)(<nil>),
+	// 	StructuredData: (*map[string]map[string]string)(<nil>),
+	// 	Message: (*string)(<nil>)
+	// 	}),
+	// 	MessageError: (error) <nil>,
+	// 	Error: (error) <nil>
+	// },
+	// (rfc5425.Result) {
+	// 	Message: (*rfc5424.SyslogMessage)({
+	// 	Priority: (*uint8)(2),
+	// 	facility: (*uint8)(0),
+	// 	severity: (*uint8)(2),
+	// 	Version: (uint16) 1,
+	// 	Timestamp: (*time.Time)(<nil>),
+	// 	Hostname: (*string)("host.local"),
+	// 	Appname: (*string)("su"),
+	// 	ProcID: (*string)(<nil>),
+	// 	MsgID: (*string)(<nil>),
+	// 	StructuredData: (*map[string]map[string]string)(<nil>),
+	// 	Message: (*string)((len=11) "κόσμε")
+	// 	}),
+	// 	MessageError: (error) <nil>,
+	// 	Error: (error) <nil>
+	// }
+	// }
+}
+
+func EmittingParsing(p *Parser) chan Result {
+	c := make(chan Result)
+
+	toChannel := func(r *Result) {
+		c <- *r
+	}
+
+	go func() {
+		defer close(c)
+		p.ParseExecuting(toChannel)
+	}()
+
+	return c
+}
+
+func ExampleParseSendingToChannel() {
+	r := strings.NewReader("16 <1>1 - - - - - -17 <2>12 A B C D E -16 <1>1")
+	results := EmittingParsing(NewParser(r, WithBestEffort()))
+
+	for r := range results {
+		fmt.Println(r)
+	}
+}
