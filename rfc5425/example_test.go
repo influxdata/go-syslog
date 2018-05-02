fad38c919262d1d3577dffd6a8028a6ed333dd62
Leonardo Di Donato <leodidonato@gmail.com>
2018-05-02 15:24:17 +0200

    Update: Example of parse handler sending to channel

--- rfc5425/example_test.go
+++ rfc5425/example_test.go
@@ -1,13 +1,12 @@
 package rfc5425
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
 )
 
-func output(out []Result) {
+func output(out interface{}) {
 	spew.Config.DisableCapacities = true
 	spew.Config.DisablePointerAddresses = true
 	spew.Dump(out)
@@ -87,11 +86,63 @@ func EmittingParsing(p *Parser) chan Result {
 	return c
 }
 
-func ExampleParseSendingToChannel() {
+func ExampleParser() {
 	r := strings.NewReader("16 <1>1 - - - - - -17 <2>12 A B C D E -16 <1>1")
 	results := EmittingParsing(NewParser(r, WithBestEffort()))
 
 	for r := range results {
-		fmt.Println(r)
+		output(r)
 	}
+	// Output:
+	// (rfc5425.Result) {
+	//  Message: (*rfc5424.SyslogMessage)({
+	//   Priority: (*uint8)(1),
+	//   facility: (*uint8)(0),
+	//   severity: (*uint8)(1),
+	//   Version: (uint16) 1,
+	//   Timestamp: (*time.Time)(<nil>),
+	//   Hostname: (*string)(<nil>),
+	//   Appname: (*string)(<nil>),
+	//   ProcID: (*string)(<nil>),
+	//   MsgID: (*string)(<nil>),
+	//   StructuredData: (*map[string]map[string]string)(<nil>),
+	//   Message: (*string)(<nil>)
+	//  }),
+	//  MessageError: (error) <nil>,
+	//  Error: (error) <nil>
+	// }
+	// (rfc5425.Result) {
+	//  Message: (*rfc5424.SyslogMessage)({
+	//   Priority: (*uint8)(2),
+	//   facility: (*uint8)(0),
+	//   severity: (*uint8)(2),
+	//   Version: (uint16) 12,
+	//   Timestamp: (*time.Time)(<nil>),
+	//   Hostname: (*string)(<nil>),
+	//   Appname: (*string)(<nil>),
+	//   ProcID: (*string)(<nil>),
+	//   MsgID: (*string)(<nil>),
+	//   StructuredData: (*map[string]map[string]string)(<nil>),
+	//   Message: (*string)(<nil>)
+	//  }),
+	//  MessageError: (*errors.errorString)(expecting a RFC3339 or a RFC3339NANO timestamp or a nil value [col 6]),
+	//  Error: (error) <nil>
+	// }
+	// (rfc5425.Result) {
+	//  Message: (*rfc5424.SyslogMessage)({
+	//   Priority: (*uint8)(1),
+	//   facility: (*uint8)(0),
+	//   severity: (*uint8)(1),
+	//   Version: (uint16) 1,
+	//   Timestamp: (*time.Time)(<nil>),
+	//   Hostname: (*string)(<nil>),
+	//   Appname: (*string)(<nil>),
+	//   ProcID: (*string)(<nil>),
+	//   MsgID: (*string)(<nil>),
+	//   StructuredData: (*map[string]map[string]string)(<nil>),
+	//   Message: (*string)(<nil>)
+	//  }),
+	//  MessageError: (*errors.errorString)(parsing error [col 4]),
+	//  Error: (*errors.errorString)(found EOF after "<1>1", expecting a SYSLOGMSG containing 16 octets)
+	// }
 }
