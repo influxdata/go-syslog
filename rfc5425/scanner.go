4cd2eaed5c513958355fb97f6b74b8e3cd174a45
Leonardo Di Donato <leodidonato@gmail.com>
2018-05-10 21:23:32 +0200

    Fix: As per RFC5425#4.3.1 receivers SHOULD be able to process messages with lengths up to and including 8192 octets

--- rfc5425/scanner.go
+++ rfc5425/scanner.go
@@ -7,6 +7,9 @@ import (
 	"strconv"
 )
 
+// size as per rfc5425#section-4.3.1
+var size = 8192
+
 // eof represents a marker byte for the end of the reader
 var eof = byte(0)
 
@@ -33,7 +36,7 @@ type Scanner struct {
 // NewScanner returns a pointer to a new instance of Scanner.
 func NewScanner(r io.Reader) *Scanner {
 	return &Scanner{
-		r: bufio.NewReader(r),
+		r: bufio.NewReaderSize(r, size+5), // "8192 " has length 5
 	}
 }
 
@@ -109,6 +112,9 @@ func (s *Scanner) scanMsgLen() Token {
 
 	msglen := buf.String()
 	s.msglen, _ = strconv.ParseUint(msglen, 10, 64)
+
+	// (todo) > return ILLEGAL if s.msglen > size (8192)
+	// (todo) > only when NOT in besteffort mode or always?
 
 	return Token{
 		typ: MSGLEN,
