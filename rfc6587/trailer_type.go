ce3912b0e2a97c1498559d91afa70e788216a099
Leonardo Di Donato <leodidonato@gmail.com>
2018-12-12 09:43:07 +0100

    New: Trailer types for RFC6587

---
+++ rfc6587/trailer_type.go
@@ -0,0 +1,34 @@
+package rfc6587
+
+import (
+	"fmt"
+)
+
+// TrailerType is the king of supported trailers for non-transparent frames.
+type TrailerType int
+
+const (
+	// LF is the line feed - ie., byte 10. Also the default one.
+	LF TrailerType = iota
+	// NUL is the nul byte - ie., byte 0.
+	NUL
+)
+
+var names = [...]string{"LF", "NUL"}
+var bytes = []int{10, 0}
+
+func (t TrailerType) String() string {
+	if t < LF || t > NUL {
+		return ""
+	}
+
+	return names[t]
+}
+
+func (t TrailerType) Value() (int, error) {
+	if t < LF || t > NUL {
+		return -1, fmt.Errorf("unknown TrailerType")
+	}
+
+	return bytes[t], nil
+}
