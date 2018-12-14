b97990aa4c56f61cbcfc1e54e49b26ae12b2ecf8
Leonardo Di Donato <leodidonato@gmail.com>
2018-12-14 11:09:02 +0100

    New: Trailer type tests

---
+++ rfc6587/trailer_type_test.go
@@ -0,0 +1,49 @@
+package rfc6587
+
+import (
+	"encoding/json"
+
+	"github.com/stretchr/testify/assert"
+
+	"testing"
+)
+
+type trailerWrapper struct {
+	Trailer TrailerType `json:"trailer"`
+}
+
+func TestUnmarshalLowercase(t *testing.T) {
+	x := &trailerWrapper{}
+	in := []byte(`{"trailer": "lf"}`)
+	err := json.Unmarshal(in, x)
+	assert.Nil(t, err)
+	assert.Equal(t, &trailerWrapper{Trailer: LF}, x)
+}
+
+func TestUnmarshalUnknown(t *testing.T) {
+	x := &trailerWrapper{}
+	in := []byte(`{"trailer": "UNK"}`)
+	err := json.Unmarshal(in, x)
+	assert.Error(t, err)
+	assert.Equal(t, &trailerWrapper{Trailer: -1}, x)
+}
+
+func TestUnmarshal(t *testing.T) {
+	x := &trailerWrapper{}
+	in := []byte(`{"trailer": "NUL"}`)
+	err := json.Unmarshal(in, x)
+	assert.Nil(t, err)
+	assert.Equal(t, &trailerWrapper{Trailer: NUL}, x)
+}
+
+func TestMarshalUnknown(t *testing.T) {
+	res, err := json.Marshal(&trailerWrapper{Trailer: TrailerType(-2)})
+	assert.Error(t, err)
+	assert.Empty(t, res)
+}
+
+func TestMarshal(t *testing.T) {
+	res, err := json.Marshal(&trailerWrapper{Trailer: NUL})
+	assert.Nil(t, err)
+	assert.Equal(t, `{"trailer":"NUL"}`, string(res))
+}
