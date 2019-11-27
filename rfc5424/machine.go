0b3c24e30cb326e62e60a6d6c6696bd89eb167c3
Leonardo Di Donato <leodidonato@gmail.com>
2019-11-27 14:02:01 +0100

    Update: RFC5424

--- rfc5424/machine.go
+++ rfc5424/machine.go
@@ -77,6 +77,7 @@ func NewMachine(options ...syslog.MachineOption) syslog.Machine {
 	return m
 }
 
+// WithBestEffort enables best effort mode.
 func (m *machine) WithBestEffort() {
 	m.bestEffort = true
 }
