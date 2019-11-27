9d30e511f28e72f3acb9da860db18c305364da57
Leonardo Di Donato <leodidonato@gmail.com>
2019-11-27 14:44:17 +0100

    New: Best effort option for RFC3164

---
+++ rfc3164/options.go
@@ -0,0 +1,13 @@
+package rfc3164
+
+import (
+	syslog "github.com/influxdata/go-syslog/v2"
+)
+
+// WithBestEffort enables the best effort mode.
+func WithBestEffort() syslog.MachineOption {
+	return func(m syslog.Machine) syslog.Machine {
+		m.WithBestEffort()
+		return m
+	}
+}
