0167402be73c557b0d17f8504bcdef3283b82bef
Leonardo Di Donato <leodidonato@gmail.com>
2019-11-27 14:50:30 +0100

    New: RFC3164 FSM tests setup

---
+++ rfc3164/machine_test.go
@@ -0,0 +1,57 @@
+package rfc3164
+
+import (
+	"testing"
+
+	"github.com/influxdata/go-syslog/v2"
+	"github.com/influxdata/go-syslog/v2/rfc5424"
+	syslogtesting "github.com/influxdata/go-syslog/v2/testing"
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	input        []byte
+	valid        bool
+	value        syslog.Message
+	errorString  string
+	partialValue syslog.Message
+}
+
+var testCases = []testCase{
+	{
+		[]byte(`<34>Jan 12 06:30:00 xxx apache_server: 1.2.3.4 - - [12/Jan/2011:06:29:59 +0100] "GET /foo/bar.html HTTP/1.1" 301 96 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; fr; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12 ( .NET CLR 3.5.30729)" PID 18904 Time Taken 0`),
+		true,
+		&rfc5424.SyslogMessage{},
+		"",
+		&rfc5424.SyslogMessage{},
+	},
+}
+
+func TestMachineParse(t *testing.T) {
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(syslogtesting.RightPad(string(tc.input), 50), func(t *testing.T) {
+			t.Parallel()
+
+			message, merr := NewMachine().Parse(tc.input)
+			partial, perr := NewMachine(WithBestEffort()).Parse(tc.input)
+
+			if !tc.valid {
+				assert.Nil(t, message)
+				assert.Error(t, merr)
+				assert.EqualError(t, merr, tc.errorString)
+
+				assert.Equal(t, tc.partialValue, partial)
+				assert.EqualError(t, perr, tc.errorString)
+			}
+			if tc.valid {
+				assert.Nil(t, merr)
+				assert.NotEmpty(t, message)
+				assert.Equal(t, message, partial)
+				assert.Equal(t, merr, perr)
+			}
+
+			assert.Equal(t, tc.value, message)
+		})
+	}
+}
