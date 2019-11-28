559d221570caeea8f381b6f116c96291c86698f8
Leonardo Di Donato <leodidonato@gmail.com>
2019-11-28 04:50:44 +0100

    Update: RFC3164 syslog message

--- rfc3164/syslog_message.go
+++ rfc3164/syslog_message.go
@@ -3,7 +3,7 @@ package rfc3164
 import (
 	"time"
 
-	"github.com/influxdata/go-syslog/v2/rfc5424"
+	"github.com/influxdata/go-syslog/v2/common"
 )
 
 type syslogMessage struct {
@@ -11,7 +11,6 @@ type syslogMessage struct {
 	timestampSet   bool // We explictly flag the setting of timestamp since its zero value is a valid timestamp by RFC 3164
 	hasElements    bool
 	priority       uint8
-	version        uint16
 	timestamp      time.Time
 	hostname       string
 	tag            string
@@ -21,15 +20,69 @@ type syslogMessage struct {
 	message        string
 }
 
-func (sm *syslogMessage) valid() bool {
-	if sm.prioritySet {
-		return true
-	}
-
-	return false
+func (sm *syslogMessage) minimal() bool {
+	return sm.prioritySet && common.ValidPriority(sm.priority)
 }
 
-func (sm *syslogMessage) export() *rfc5424.SyslogMessage {
-	// todo(leodido) > to be implemented
-	return &rfc5424.SyslogMessage{}
+// export is meant to be called on minimally-valid messages
+// thus it presumes priority and version values exists and are correct
+func (sm *syslogMessage) export() *SyslogMessage {
+	out := &SyslogMessage{}
+	out.setPriority(sm.priority)
+
+	if sm.timestampSet {
+		out.timestamp = &sm.timestamp
+	}
+
+	return out
+}
+
+// SyslogMessage represents a RFC3164 syslog message.
+type SyslogMessage struct {
+	priority       *uint8
+	facility       *uint8
+	severity       *uint8
+	timestamp      *time.Time
+	hostname       *string
+	appname        *string
+	procID         *string
+	msgID          *string
+	structuredData *map[string]map[string]string
+	message        *string
+}
+
+func (sm *SyslogMessage) setPriority(value uint8) {
+	sm.priority = &value
+	facility := uint8(value / 8)
+	severity := uint8(value % 8)
+	sm.facility = &facility
+	sm.severity = &severity
+}
+
+// Valid tells whether the receiving SyslogMessage is well-formed or not.
+//
+// A minimally well-formed RFC3164 syslog message contains at least the priority ([1, 191] or 0).
+func (sm *SyslogMessage) Valid() bool {
+	// A nil priority or a 0 version means that the message is not valid
+	return sm.priority != nil && common.ValidPriority(*sm.priority)
+}
+
+// Priority returns the syslog priority or nil when not set
+func (sm *SyslogMessage) Priority() *uint8 {
+	return sm.priority
+}
+
+// Facility returns the facility code.
+func (sm *SyslogMessage) Facility() *uint8 {
+	return sm.facility
+}
+
+// Severity returns the severity code.
+func (sm *SyslogMessage) Severity() *uint8 {
+	return sm.severity
+}
+
+// Timestamp returns the syslog timestamp or nil when not set
+func (sm *SyslogMessage) Timestamp() *time.Time {
+	return sm.timestamp
 }
