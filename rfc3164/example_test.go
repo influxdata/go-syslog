d2721beb68f726eb3a97e0874184b84e26f4e0c4
Leonardo Di Donato <leodidonato@gmail.com>
2020-01-23 01:53:28 +0100

    docs(rfc3164): example using current year strategy
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- rfc3164/example_test.go
+++ rfc3164/example_test.go
@@ -30,3 +30,24 @@ func Example() {
 	//  }
 	// })
 }
+
+func Example_currentyear() {
+	i := []byte(`<13>Dec  2 16:31:03 host app: Test`)
+	p := NewParser(WithYear(CurrentYear{}))
+	m, _ := p.Parse(i)
+	output(m)
+	// Output:
+	// (*rfc3164.SyslogMessage)({
+	//  Base: (syslog.Base) {
+	//   Facility: (*uint8)(1),
+	//   Severity: (*uint8)(5),
+	//   Priority: (*uint8)(13),
+	//   Timestamp: (*time.Time)(2020-12-02 16:31:03 +0000 UTC),
+	//   Hostname: (*string)((len=4) "host"),
+	//   Appname: (*string)((len=3) "app"),
+	//   ProcID: (*string)(<nil>),
+	//   MsgID: (*string)(<nil>),
+	//   Message: (*string)((len=4) "Test")
+	//  }
+	// })
+}
